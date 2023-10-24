dc28400b1d06ebc1ccac08b92922f70e907bace5
ImKunYoung <ur2kunyoung2@outlook.com>
2023-10-24 20:09:22 +0900

    initial commit

---
+++ src/main/java/com/example/slf4jpoc/Slf4jPocApplication.java
@@ -0,0 +1,13 @@
+package com.example.slf4jpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Slf4jPocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Slf4jPocApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/slf4jpoc/Slf4jPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.slf4jpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Slf4jPocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
